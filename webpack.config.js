4aa6759b9e5bb74a4e52abe2c1bec4503581e16a
亨睿 <henry.lx@alibaba-inc.com>
2018-04-10 12:11:42 +0800

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+const ASSETS_SOURCE_PATH = path.resolve('./src');
+const ASSETS_BUILD_PATH = path.resolve('./build');
+const ASSETS_PUBLIC_PATH = '/assets';
+
+module.exports = {
+  context: ASSETS_SOURCE_PATH,
+  entry: {
+    index: ['./index.js', './index.less']
+  },
+  output: {
+    path: ASSETS_BUILD_PATH,
+    publicPath: ASSETS_PUBLIC_PATH,
+    filename: './[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.png$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'url-loader',
+            options:
+            {
+              limit: 8192,
+              mimetype: 'image/png',
+              name: 'images/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
