2d1c218b2d11c430fc0024008ef68ef39a9771f7
亨睿 <henry.lx@alibaba-inc.com>
2018-04-11 12:30:07 +0800

    Add handleStateChange()

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import Runner from './tr/Runner';
 
 import './tr/index.less';
 
+let runner = null;
+
+function handleStateChange(state) {
+  if (!state.jumping) {
+    const jump = Math.random() > 0.97;
+    if (jump) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  const runner = new Runner('.interstitial-wrapper');
+  runner = new Runner('.interstitial-wrapper', { onStateChange: handleStateChange });
   window.runner = runner;
   runner.init();
 });
