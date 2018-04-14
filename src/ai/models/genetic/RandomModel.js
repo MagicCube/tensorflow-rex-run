d0367f8d3a51f82ed7d49d7d4c5224ed7e2e69b3
Henry Li <henry1943@163.com>
2018-04-14 19:33:28 +0800

    Add get/setChromosome()

--- src/ai/models/genetic/RandomModel.js
+++ src/ai/models/genetic/RandomModel.js
@@ -1,5 +1,14 @@
 import RandomModel from '../random/RandomModel';
 
 export default class GeneticRandomModel extends RandomModel {
+  getChromosome() {
+    return this.weights.concat(this.biases);
+  }
 
+  setChromosome(chromosome) {
+    this.weights[0] = chromosome[0];
+    this.weights[1] = chromosome[1];
+    this.weights[2] = chromosome[2];
+    this.biases[0] = chromosome[3];
+  }
 }
